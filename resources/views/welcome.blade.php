0ecd8b69f08708ec8878439a618a962dec6232a2
Rayane <rayane.gueddouche@epitech.eu>
2025-12-16 16:35:37 +0100

    activite 13

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,18 +5,10 @@
 @section('content')
     <h2>Bienvenue sur la page d’accueil</h2>
 
-    <x-article
-        title="L’IA soigne mieux"
-        description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite."
-    />
-
-    <x-article
-        title="Villes vertes"
-        description="Les métropoles deviennent plus écologiques et durables."
-    />
-
-    <x-article
-        title="Télétravail"
-        description="Plus de liberté, mais aussi plus de solitude."
-    />
+    @foreach ($articles as $article)
+        <x-article
+            :title="$article['title']"
+            :description="$article['description']"
+        />
+    @endforeach
 @endsection
