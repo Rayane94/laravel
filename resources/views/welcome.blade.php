b7b0aa471961d42dd41d4cf0c9ce67ad7252b8a9
Rayane <rayane.gueddouche@epitech.eu>
2025-12-18 15:54:50 +0100

    e

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,29 @@
-@foreach ($articles as $article)
-    @if ($loop->last)
-        @break
-    @endif
+@extends('layouts.app')
 
-    <a href="{{ route('article.details', ['id' => $article->id]) }}">
-        <x-article :title="$article->title" :description="$article->description" />
-    </a>
-@endforeach
+@section('title', 'Accueil')
+
+@section('content')
+    <h2>Bienvenue sur le site de {{ $name }}</h2>
+    <p>Voici les derniers articles :</p>
+
+    <p>
+        <a href="{{ route('articles.create') }}">Créer les 3 articles</a>
+    </p>
+
+    @forelse ($articles as $article)
+        @if ($loop->last)
+            @break
+        @endif
+
+        <a href="{{ route('article.details', ['id' => $article->id]) }}">
+            <x-article :title="$article->title" :description="$article->description" />
+        </a>
+
+        <p>
+            <a href="{{ route('articles.edit', ['id' => $article->id]) }}">Modifier</a>
+            <a href="{{ route('articles.delete', ['id' => $article->id]) }}">Supprimer</a>
+        </p>
+    @empty
+        <p>Aucun article en base. Clique sur “Créer les 3 articles”.</p>
+    @endforelse
+@endsection
